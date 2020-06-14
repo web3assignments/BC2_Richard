24a04dce6db14c2f52629bd5d0a7e3e19f979549
Riezzie_3 <richardkerkvliet@hotmail.com>
2020-06-14 12:03:06 +0200

    role controlled coinflip

--- Day_11/contracts/CoinFlip.sol
+++ Day_11/contracts/CoinFlip.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.6.0;
 import "github.com/provable-things/ethereum-api/provableAPI_0.6.sol";
 import "github.com/OpenZeppelin/openzeppelin-contracts/contracts/math/SafeMath.sol";
 import "./Mortal.sol";
+import "./RoleControlled.sol";
 
-contract CoinFlip is Mortal, usingProvable {
+contract CoinFlip is usingProvable, RoleControlled, Mortal {
     using SafeMath for uint256;
 
     struct Game {
@@ -14,11 +15,21 @@ contract CoinFlip is Mortal, usingProvable {
     }
 
     mapping(bytes32 => Game) private pendingGames;
-    mapping(address => uint256) public playerLosses;
-    mapping(address => uint256) public playerWinings;
+    mapping(address => uint256) private playerLosses;
+    mapping(address => uint256) private playerWinings;
 
     event Result(bool winner, uint256 winnings);
     event LogNewProvableQuery(string description);
+
+    modifier minimumBet(uint256 bet) {
+        require(msg.value >= bet, "Minimum bet not reached");
+        _;
+    }
+
+    modifier allowedChoices(uint8 _choice) {
+        require(_choice == 0 || _choice == 1, "Choice must either be 1 or 0");
+        _;
+    }
 
     constructor() public payable {}
 
@@ -26,17 +37,34 @@ contract CoinFlip is Mortal, usingProvable {
 
     receive() external payable {}
 
-    function flip(uint8 _choice) public payable {
-        require(msg.value > 0.2 ether, "Bet must be at least 0.2 Ether");
+    function flip(uint8 _choice) public payable minimumBet(0.2 ether) allowedChoices(_choice) minimumRole(Roles.FREE) {
         require(
             msg.value * 2 <= address(this).balance,
             "Contract can't pay out that kind of money right now"
         );
-        require(_choice == 0 || _choice == 1, "Choice must either be 1 or 0");
 
         bytes32 queryId = provable_newRandomDSQuery(0, 1, 2000000);
         emit LogNewProvableQuery("genereated query");
         pendingGames[queryId] = Game(msg.sender, _choice, msg.value);
+    }
+
+    function getMyLosses() public minimumRole(Roles.USER) returns (uint256 losses) {
+        return playerLosses[msg.sender];
+    }
+
+    function getMyWinings() public minimumRole(Roles.USER) returns (uint256 winings) {
+        return playerWinings[msg.sender];
+    }
+
+    function resetPlayer(address _player) public minimumRole(Roles.ADMIN) returns (bool success){
+        delete playerLosses[_player];
+        delete playerWinings[_player];
+        delete userRoles[_player];
+
+        if (playerLosses[_player] == 0 && playerWinings[_player] == 0 && userRoles[_player] == Roles.FREE) {
+            return true;
+        }
+        return false;
     }
 
     function __callback(bytes32 _queryId, string memory _result)
